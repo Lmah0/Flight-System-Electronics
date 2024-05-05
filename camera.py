e7aae51b477de83945e7526f245387bce5a742eb
Liam Mah <liam.mah@ucalgary.ca>
2024-05-05 12:40:06 -0600

    Adding GCS URL command line argument.

--- camera.py
+++ camera.py
@@ -6,6 +6,9 @@ from exif import Image
 import piexif
 from flask import Flask
 import threading
+import requests
+
+# Built to sync with GCS2024 in SUAV repo
 
 app = Flask(__name__)
 
@@ -13,6 +16,7 @@ cameraState = 0  # 0: off, 1: on
 pictureId = 0
 picam2 = None
 lock = threading.Lock() # Thread locking for synchronizing shares resources
+gcsUrl = None
 
 @app.route('/cameraOn', methods=['POST'])
 def triggerCamera():
@@ -27,7 +31,7 @@ def triggerCamera():
         cameraState = 1
 
     while cameraState == 1:
-        takePicture()
+        takeAndSendPicture()
 
 @app.route('/cameraOff', methods=['POST'])
 def stopCamera():
@@ -35,7 +39,7 @@ def stopCamera():
     with lock:
         cameraState = 0
 
-def takePicture():
+def takeAndSendPicture():
     global pictureId, picam2
     with lock:
         directory = "/Users/liammah/Desktop/University/Schulich UAV/Electronics"
@@ -44,6 +48,13 @@ def takePicture():
         filepath = f"{directory}/{filename}"
         image = picam2.capture_image("main")
         image.save(filepath, None)
+        
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 camera.py <GCS_URL>")
+        sys.exit(1)
+    
+    gcsUrl = sys.argv[1]
     app.run(debug=True, host='0.0.0.0', threaded=True)
+
