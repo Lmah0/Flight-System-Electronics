7e98fb381765254786d9dd96943a14cd5ce093ff
Liam Mah <liam.mah@ucalgary.ca>
2024-06-07 22:45:08 -0600

    Adding functionality to take a single picture in real time for real time location reading.

--- SUAS2024.py
+++ SUAS2024.py
@@ -263,6 +263,20 @@ def take_and_send_picture_no_local(i, picam2):
 
     return time.time()
 
+@app.route('/locator', methods=['GET'])
+def picture_locator():
+    image_stream = BytesIO()
+    image = picam2.capture_image('main')
+    image.save(image_stream, format='JPEG')
+    image_stream.seek(0)
+
+    image_file = {
+        'file': (f'locator.jpg', image_stream, 'image/jpeg'),
+    }
+
+    headers = {}
+    response = requests.request("POST", f"{gcs_url}/submit", headers=headers, files=image_file)
+
 def receive_vehicle_position():  # Actively runs and receives live vehicle data on a separate thread
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
